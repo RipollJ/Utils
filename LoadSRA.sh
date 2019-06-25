f29420f75ebc140f236bd6cce09c9dd68026d462
Ripoll <julie.ripoll87@gmail.com>
2019-06-25 18:48:48 +0000

    Update LoadDump.sh >> LoadSRA.sh

--- LoadSRA.sh
+++ LoadSRA.sh
@@ -15,24 +15,28 @@
 #######################################################################
 # Pre-requisite:
    # Use: conda for installation (or not), sra-toolkits and parallel-fastq-dump
+   # conda install -c bioconda sra-tools
+   # conda install -c bioconda parallel-fastq-dump
 
 
 #######################################################################
-# droit execution: chmod +x LoadDump.sh
-# execution ./LoadDump.sh ListOfIds.txt {number of threads to use}
+# droit execution: chmod +x LoadSRA.sh
+# execution ./LoadSRA.sh ListOfIds.txt {number of threads to use}
+# Caution: the double of the value given in threads will be used due to parallel
 # ListOfIds.txt: your file with SRA Identifiants to download, one file by raw
 
-#file = $1
+function fline {
+  cat $1 | parallel -j 2 "FastDump {}"
+}; export -f fline
 
-function LoadDump {
-  #v= echo $(cat $1)
+function FastDump {
   prefetch -O ./ -X 999999999 $1
 
   if [[ -e ${1}.sra ]]; then
-    parallel-fastq-dump -s ${1}.sra -t 8 -O ./ --tmpdir ./ --split-3 --gzip && rm ${1}.sra
+    parallel-fastq-dump -s ${1}.sra -t $2 -O ./ --tmpdir ./ --split-3 --gzip && rm ${1}.sra
   else
     echo '[ERROR]' $1 'apparently not successfully loaded' && exit 1
   fi
-}; export -f LoadDump
+}; export -f FastDump
 
-cat ListOfIds.txt | parallel -j 2 "LoadDump {}"
+fline $1
